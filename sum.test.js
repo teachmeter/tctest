d0f9adea4fe82fba8b4c6771775f0bf670f91bfd
Lucas Chmaj <teachmeter@gmail.com>
2021-01-21 00:42:08 +0100

    Additional tests

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,13 @@
 const sum = require('./sum');
 
+test('Method exists', () => {
+    expect(sum).toBeInstanceOf(Function)
+})
+
 test('adds 1+2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
+})
+
+test('adds 1+1 to not equal 3', () => {
+    expect(sum(1,1)).not.toBe(3)
 })
